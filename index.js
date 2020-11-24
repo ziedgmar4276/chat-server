50cc400445c1a6f15f3aa1a02fb552f1a2226e59
ziedgmar <ziedgmar242@gmail.com>
2020-11-24 21:17:37 +0100

    nom chat

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ io.on('connect', (socket) => {
 
     socket.join(user.room);
 
-    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.emit('message', { user: 'ROBOT', text: `${user.name},bienvenue dans le group ${user.room}.`});
     socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
